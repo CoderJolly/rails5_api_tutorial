f6f8fdc57242376adc8f0e7275f4710c090e14b7
Filippos Vasilakis <vasilakisfil@gmail.com>
2018-09-30 15:18:38 +0200

    fixes

--- app/serializers/api/v1/user_serializer.rb
+++ app/serializers/api/v1/user_serializer.rb
@@ -5,6 +5,8 @@ class Api::V1::UserSerializer < Api::V1::BaseSerializer
   attribute :following_state
   attribute :follower_state
 
+  collection :users
+
   def following_state
     return false unless current_user
 
@@ -21,19 +23,6 @@ class Api::V1::UserSerializer < Api::V1::BaseSerializer
       follower_id: object.id,
       followed_id: current_user.id
     ).exists?
-  end
-
-  collection :users do
-    meta :current_page
-    meta :next_page
-    meta :prev_page
-    meta :total_count
-    meta :total_pages
-
-
-    link :self, ->(obj, s){ "http://localhost:3000/api/v1/users/?page%5Bnumber%5D=1&page%5Bsize%5D=25" }
-    link :next, ->(obj, s){ "http://localhost:3000/api/v1/users/?page%5Bnumber%5D=2&page%5Bsize%5D=25" }
-    link :last, ->(obj, s){ "http://localhost:3000/api/v1/users/?page%5Bnumber%5D=4&page%5Bsize%5D=25" }
   end
 
   has_one :feed, serializer: Api::V1::MicropostSerializer do
